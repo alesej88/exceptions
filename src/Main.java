9288145e75bcea008872e1ad2b4aef4e0aaa669a
alesej88 <alesej88@ya.ru>
2021-04-28 11:24:37 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+
+    import java.util.Arrays;
+
+    public class Main {
+
+        /**
+         * 1. Напишите метод, на вход которого подаётся двумерный строковый массив размером 4х4,
+         * при подаче массива другого размера необходимо бросить исключение MyArraySizeException.
+         * <p>
+         * 2. Далее метод должен пройтись по всем элементам массива, преобразовать в int,
+         * и просуммировать. Если в каком-то элементе массива преобразование не удалось (например,
+         * в ячейке лежит символ или текст вместо числа), должно быть брошено исключение MyArrayDataException,
+         * с детализацией в какой именно ячейке лежат неверные данные.
+         *
+         * @param arr Строковый массив 4x4
+         * @return boolean
+         */
+        private static int sumAllStrings(String[][] arr) throws MyArraySizeException, MyArrayDataException {
+
+            int sum = 0;
+            if (arr.length != 4) throw new MyArraySizeException();
+
+            for (int i = 0; i <= arr.length - 1; i++) {
+                if (arr[i].length != 4) throw new MyArraySizeException();
+                for (int j = 0; j <= arr[i].length - 1; j++) {
+                    try {
+                        sum += Integer.parseInt(arr[i][j]);
+                    } catch (NumberFormatException e) {
+                        throw new MyArrayDataException(i, j);
+                    }
+                }
+            }
+
+            return sum;
+        }
+
+    }
